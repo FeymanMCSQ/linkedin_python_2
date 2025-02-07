c832c8c4553fbdcfe57a3095e8cf4cdb274f7ea4
Safi Ullah <150228504+FeymanMCSQ@users.noreply.github.com>
2025-02-07 16:08:45 +0500

    Added new Article

--- github_commit.py
+++ github_commit.py
@@ -1,14 +1,13 @@
 import os
 
 def github_commit():
-    GITHUB_REPO = "https://github.com/FeymanMCSQ/linkedin-python.git"
+    GITHUB_REPO = "https://github.com/FeymanMCSQ/linkedin_python_2.git"
     COMMIT_MESSAGE = "Added new Article"
 
+    os.system(f"git remote add origin {GITHUB_REPO}")
     # Ensure we are on the main branch (create it if it doesn't exist)
     os.system("git checkout main || git checkout -b main")
 
-    # Pull latest changes and auto-merge (to prevent non-fast-forward errors)
-    # os.system("git pull --rebase --autostash origin main || git reset --hard origin/main")
 
     # Add changes and commit
     os.system("git add .")
